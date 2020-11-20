bd8fa4de07b20fe553c6f1b7aa58831bf7c778a3
Xiangyang <nicolas.te@hotmail.fr>
2020-11-20 10:59:41 +0100

    added room imports

---
+++ app/src/main/java/fr/yncrea/anfroid_project/GetJsonApi.java
@@ -0,0 +1,10 @@
+package fr.yncrea.anfroid_project;
+
+import java.util.List;
+
+// import fr.yncrea.roomdemo.model.StarWarsPerson;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface GetJsonApi {
+}
--- app/src/main/java/fr/yncrea/anfroid_project/MainActivity.java
+++ app/src/main/java/fr/yncrea/anfroid_project/MainActivity.java
@@ -1,14 +1,31 @@
 package fr.yncrea.anfroid_project;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+import retrofit2.Retrofit;
+import fr.yncrea.anfroid_project.GetJsonApi;
 
 public class MainActivity extends AppCompatActivity {
 
+    private GetJsonApi eventsList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://swapi.dev/api/")
+            .build();
+
+    eventsList = retrofit.create(getJsonApi.class);
 }
