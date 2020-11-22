d8938a89b97f2a16b70f5abf82cef95acf612934
JarJarBinksIsen <36074939+JarJarBinksIsen@users.noreply.github.com>
2020-11-22 11:57:37 +0100

    ListView et GridView

--- app/src/main/java/fr/yncrea/anfroid_project/MainActivity.java
+++ app/src/main/java/fr/yncrea/anfroid_project/MainActivity.java
@@ -4,54 +4,107 @@
  */
 package fr.yncrea.anfroid_project;
 
-import androidx.annotation.XmlRes;
 
+import android.app.Activity;
 import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.recyclerview.widget.RecyclerView;
-
-
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-import java.util.stream.Collectors;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
-import fr.yncrea.anfroid_project.model.EventsList;
-import fr.yncrea.anfroid_project.api.GetEvents;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
-    ConstraintLayout layout = null;
-    TextView text = null;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        layout = (ConstraintLayout) ConstraintLayout.inflate(this, R.layout.activity_main, null);
+        //setContentView(R.layout.activity_main);
 
+        Cheese[] cheeses = {
+                new Cheese("Parmesan", "Hard, granular cheese"),
+                new Cheese("Ricotta", "Italian whey cheese"),
+                new Cheese("Fontina", "Italian cow's milk cheese"),
+                new Cheese("Mozzarella", "Southern Italian buffalo milk cheese"),
+                new Cheese("Cheddar", "Firm, cow's milk cheese"),
+        };
+        ArrayAdapter<Cheese> cheeseAdapter =
+                new ArrayAdapter<Cheese>(this, 0, cheeses) {
+                    @Override
+                    public View getView(int position,
+                                        View convertView,
+                                        ViewGroup parent) {
+                        Cheese currentCheese = cheeses[position];
+                        // Inflate only once
+                        if(convertView == null) {
+                            convertView = getLayoutInflater()
+                                    .inflate(R.layout.custom_item, null, false);
+                        }
+                        ViewHolder viewHolder = new ViewHolder();
+                        viewHolder.cheeseName =
+                                (TextView)convertView.findViewById(R.id.cheese_name);
+                        viewHolder.cheeseDescription =
+                                (TextView)convertView.findViewById(R.id.cheese_description);
 
-        setContentView(R.layout.activity_main);
+                        // Store results of findViewById
+                        convertView.setTag(viewHolder);
+                        TextView cheeseName =
+                                ((ViewHolder)convertView.getTag()).cheeseName;
+                        TextView cheeseDescription =
+                                ((ViewHolder)convertView.getTag()).cheeseDescription;
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://dasnesel.github.io/AndroidStory/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+                        cheeseName.setText(currentCheese.name);
+                        cheeseDescription.setText(currentCheese.description);
+                        return convertView;
+                    }
+                };
+        ListView cheeseList = new ListView(this);
+        setContentView(cheeseList);
+        cheeseList.setAdapter(cheeseAdapter);
 
-        GetEvents eventsList = retrofit.create(GetEvents.class);
-        //RecyclerView recyclerView = (RecyclerView) findViewById(R.id.buttonSuite);
+        /** 5. Creating a Grid */
+
+        GridView cheeseGrid = new GridView(this);
+        setContentView(cheeseGrid);
+        cheeseGrid.setNumColumns(2);
+        cheeseGrid.setColumnWidth(60);
+        cheeseGrid.setVerticalSpacing(20);
+        cheeseGrid.setHorizontalSpacing(20);
+        cheeseGrid.setAdapter(cheeseAdapter);
+
+        /** 6. Adding Event Listeners */
+        cheeseGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView,
+                                    View view, int position, long rowId) {
+
+                // Generate a message based on the position
+                String message = "You clicked on " + cheeses[position].name;
+
+                // Use the message to create a Snackbar
+                Snackbar.make(adapterView, message, Snackbar.LENGTH_LONG)
+                        .show(); // Show the Snackbar
+            }
+        });
     }
 
+    static class Cheese {
+        String name;
+        String description;
 
+        public Cheese(String name, String description) {
+            this.name = name;
+            this.description = description;
+        }
+    }
+
+    static class ViewHolder{
+        TextView cheeseName;
+        TextView cheeseDescription;
+    }
 }
