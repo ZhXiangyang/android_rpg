fec4320562fc2a3d5d75790a3f664f694a282695
Xiangyang <nicolas.te@hotmail.fr>
2020-11-22 11:13:24 +0100

    changed project name

--- app/src/androidTest/java/fr/yncrea/android_rpg/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/fr/yncrea/android_rpg/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("fr.yncrea.anfroid_project", appContext.getPackageName());
+        assertEquals("fr.yncrea.android_rpg", appContext.getPackageName());
     }
 }
